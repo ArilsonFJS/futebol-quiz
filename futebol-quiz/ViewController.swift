cbaa192b70b8828e94e57db26a6b8a38dab90a0a
ArilsonFJS <arilsonsilva61@gmail.com>
2023-05-09 22:56:20 -0300

    implementacao de atualizacao das perguntas

--- futebol-quiz/ViewController.swift
+++ futebol-quiz/ViewController.swift
@@ -16,16 +16,12 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var progressBar: UIProgressView!
     
-    let quiz = ["Cristiano Ronaldo jogou no Barcelona antes de se transferir para o Real Madrid. (Falso)",
-                "Lionel Messi é o maior artilheiro da história da Liga dos Campeões da UEFA. (Verdadeiro)",
-                "Real Madrid é o time com mais títulos da Liga dos Campeões da UEFA. (Verdadeiro)",
-                "Cristiano Ronaldo venceu a Liga dos Campeões da UEFA mais vezes do que Lionel Messi. (Verdadeiro)",
-                "Lionel Messi e Cristiano Ronaldo nunca jogaram juntos em um mesmo time. (Verdadeiro)",
-                "O Barcelona venceu mais vezes a Liga dos Campeões da UEFA do que o Real Madrid. (Falso)",
-                "Cristiano Ronaldo é o jogador com mais gols em uma única temporada da Liga dos Campeões da UEFA. (Verdadeiro)",
-                "Lionel Messi já marcou mais de 100 gols na Liga dos Campeões da UEFA. (Verdadeiro)",
-                "O Real Madrid venceu a Liga dos Campeões da UEFA em três temporadas consecutivas. (Verdadeiro)",
-                "Messi venceu mais vezes o prêmio de Jogador do Ano da UEFA do que Ronaldo. (Falso)"]
+    let quiz = [
+                    ["Cristiano Ronaldo jogou no Barcelona antes de se transferir para o Real Madrid.", "Falso"],
+                    ["Lionel Messi é o maior artilheiro da história da Liga dos Campeões da UEFA.", "Falso"]
+                ]
+    
+    var numQuestao = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,14 +29,25 @@ class ViewController: UIViewController {
     }
     
     @IBAction func actionBtn(_ sender: UIButton) {
-        atualizarPerguntas()
+        
+        let respostaUser = sender.currentTitle
+        let respQuestaoAtual = quiz[numQuestao][1]
+        
+        if respostaUser == respQuestaoAtual {
+            print("OK")
+        }else{
+            print("NOK")
+        }
+        
+        if numQuestao + 1 < quiz.count {
+            numQuestao += 1
+            atualizarPerguntas()
+        }
     }
     
     func atualizarPerguntas () {
-        var numQuestao : Int = 0
         if (!quiz.isEmpty){
-            numQuestao = Int.random(in: 0...quiz.count)
-            perguntasLabel.text = quiz[numQuestao]
+            perguntasLabel.text = quiz[numQuestao][0]
         }
     }
 }
